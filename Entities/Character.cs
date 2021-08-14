b14c5fec4ede5e3e7777286b20fea9d906742276
Hovinarri <38521464+MikiPelkonen@users.noreply.github.com>
2021-08-14 20:09:11 +0300

    Character added.

---
+++ Entities/Character.cs
@@ -0,0 +1,20 @@
+﻿using RedRidingHood.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedRidingHood.Entities
+{
+    public class Character
+    {
+        Sprite sprite;
+        public Location Location { get; set; }
+        public CharacterState State { get; set; }
+        public Direction Direction { get; set; }
+        public float Depth => Location.Floor * 0.1f + Location.Row * 0.01f + 0.01f;
+
+    }
+
+    public enum CharacterState { Idle, Moving, Dead }
+    public enum Direction { North, South, East, West }
+}
